0a992eb76beba2afc36d0b14e4c2ac7b80ba21aa
kenkatou354 <kenkatou354@gmail.com>
2025-12-02 01:31:32 +0900

    [add]入力をバッファリングするクラスを追加

---
+++ Assets/FPS/InGame/Scripts/Input/InputBuffer.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.InputSystem;
+
+namespace FPS.InGame.Scripts.Player
+{
+    ///<summary> 入力バッファクラス </summary>
+    [RequireComponent(typeof(PlayerInput)), DefaultExecutionOrder(-99)]
+    public class InputBuffer
+    {
+        public InputBuffer(PlayerInput playerInput)
+        {
+            _playerInput = playerInput;
+
+        }
+        public InputActionEntity<Vector2> LookAction => _lookActionEntity;
+        public InputActionEntity<Vector2> MoveAction => _moveActionEntity;
+        public InputActionEntity<float> JumpAction => _jumpActionEntity;
+        public InputActionEntity<float> SprintAction => _sprintActionEntity;
+
+
+        [SerializeField]
+        private const string _lookActionName = "Look";
+        [SerializeField]
+        private const string _moveActionName = "Move";
+        [SerializeField]
+        private const string _jumpActionName = "Jump";
+        [SerializeField]
+        private const string _sprintActionName = "Sprint";
+
+        private PlayerInput _playerInput;
+
+        private InputActionEntity<Vector2> _lookActionEntity;
+        private InputActionEntity<Vector2> _moveActionEntity;
+        private InputActionEntity<float> _jumpActionEntity;
+        private InputActionEntity<float> _sprintActionEntity;
+
+
+        private void Awake()
+        {
+            if (_playerInput != null)
+            {
+                _playerInput.notificationBehavior = PlayerNotifications.InvokeCSharpEvents;
+                _lookActionEntity = new InputActionEntity<Vector2>(_playerInput.actions[_lookActionName]);
+                _moveActionEntity = new InputActionEntity<Vector2>(_playerInput.actions[_moveActionName]);
+                _jumpActionEntity = new InputActionEntity<float>(_playerInput.actions[_jumpActionName]);
+                _sprintActionEntity = new InputActionEntity<float>(_playerInput.actions[_sprintActionName]);
+            }
+        }
+    }
+}
