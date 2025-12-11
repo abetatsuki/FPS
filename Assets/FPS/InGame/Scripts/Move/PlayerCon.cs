902ec537d51114004a19951111ea14c249a19188
kenkatou354 <kenkatou354@gmail.com>
2025-12-11 11:48:43 +0900

    [add]コライダーと地面に接地判定を追加

--- Assets/FPS/InGame/Scripts/Move/PlayerCon.cs
+++ Assets/FPS/InGame/Scripts/Move/PlayerCon.cs
@@ -2,14 +2,19 @@ using Assets.FPS.InGame.Scripts.Interface;
 using FPS.InGame.Scripts.Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UniRx;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerCon : MonoBehaviour
 {
     private IMovarable _mover;
     private IInputrable _inputrable;
     private ILookrable _lookrable;
+    private IJumprable _jumprable;
     private AnimationCon _animCon;
+    private Collider _collider;
  
+    private readonly IntReactiveProperty _health = new IntReactiveProperty();
+
     [SerializeField] private Transform _cameraRoot;
     [SerializeField] private float _speed = 10f;
     private void Awake()
@@ -18,6 +23,8 @@ public class PlayerCon : MonoBehaviour
         _inputrable = new PlayerInputReader(GetComponent<PlayerInput>());
         _lookrable = new Look(GetComponent<Transform>(),_cameraRoot.transform);
         _animCon = new AnimationCon(GetComponent<Animator>());
+        _jumprable = new Jumper(GetComponent<Collider>());
+
     }
 
     private void Update()
@@ -29,5 +36,10 @@ public class PlayerCon : MonoBehaviour
         _lookrable.ChangeLook(_inputrable.LookInput);
         _mover.Move(_inputrable.MoveInput);
         _animCon.SetAnimation(_mover.CurrentSpeed);
+        _jumprable.Jump(_inputrable.JumpInput);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _jumprable.OnCollisionEnter(collision);
     }
 }
