0cd48c5adfa823ff71da0d3cfc463d219f579db1
kenkatou354 <kenkatou354@gmail.com>
2025-12-11 11:47:21 +0900

    [add]Lerpにスピードを追加

--- Assets/FPS/InGame/Scripts/Move/RigidBodyMover.cs
+++ Assets/FPS/InGame/Scripts/Move/RigidBodyMover.cs
@@ -12,11 +12,12 @@ public class RigidBodyMover : IMovarable
     public void Move(Vector3 direction)
     {
         Vector3 delta = MovementCalculator.Calculate(direction, _speed, Time.deltaTime, _camera);
-        _rb.linearVelocity = delta;
+        _rb.linearVelocity = Vector3.Lerp(_rb.linearVelocity,delta,_acceleration * Time.deltaTime);
         _currentSpeed = _rb.linearVelocity.magnitude;
     }
     private float _currentSpeed = 0f;
     private float _speed;
+    private float _acceleration = 2f;
     private Transform _camera;
     private Rigidbody _rb;
 }
